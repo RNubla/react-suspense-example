204a4f9ea6f5d51395aba0bbf8ae8f9d7f095c9e
Robert Nubla <rnubla97@gmail.com>
2024-09-21 17:51:39 -0400

    feat: add  skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 // const Posts = React.lazy(() => import("./component/posts"));
 import { ErrorBoundary } from "./component/ErrorBoundary";
+import { Skeleton } from "@/components/ui/skeleton";
 // import { PostForm } from "./component/FormPost";
 const PostForm = React.lazy(() => import("./component/FormPost"));
 
@@ -10,7 +11,17 @@ function App() {
 		<>
 			<div>Hi</div>
 			<ErrorBoundary>
-				<React.Suspense fallback={<div>Loading...</div>}>
+				<React.Suspense
+					fallback={
+						<div className="flex flex-col space-y-3">
+							<Skeleton className="h-[125px] w-[250px] rounded-xl" />
+							<div className="space-y-2">
+								<Skeleton className="h-4 w-[250px]" />
+								<Skeleton className="h-4 w-[200px]" />
+							</div>
+						</div>
+					}
+				>
 					{/* <Posts /> */}
 					<PostForm />
 				</React.Suspense>
